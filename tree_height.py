929b10ea0c494c2fa49af85b15079f1621edc1b6
Maksims Tihomirovs <max20im14@gmail.com>
2023-03-05 23:25:26 +0200

    221RDB154 Labb.d 2
    221RDB154 Labb.d 2

--- tree_height.py
+++ tree_height.py
@@ -1,4 +1,4 @@
-# python3
+#python3
 
 import sys
 import threading
@@ -56,5 +56,3 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
